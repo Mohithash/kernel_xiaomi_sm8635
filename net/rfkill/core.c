fdad64cd83b08a76a4063a75714f5551d55eed66
Greg Kroah-Hartman <gregkh@google.com>
2026-04-21 15:32:19 +0000

    Revert "rfkill: sync before userspace visibility/changes"
    This reverts commit f147fc8326d73055989898b19a87477f0dcccf09 which is
    commit 2c3dfba4cf84ac4f306cc6653b37b6dd6859ae9d upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I828e44b403705b2c4451ae2e9f4f2b575f52e4e7
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- net/rfkill/core.c
+++ net/rfkill/core.c
@@ -48,7 +48,6 @@ struct rfkill {
 	bool			persistent;
 	bool			polling_paused;
 	bool			suspended;
-	bool			need_sync;
 
 	const struct rfkill_ops	*ops;
 	void			*data;
@@ -367,17 +366,6 @@ static void rfkill_set_block(struct rfkill *rfkill, bool blocked)
 
 	if (prev != curr)
 		rfkill_event(rfkill);
-}
-
-static void rfkill_sync(struct rfkill *rfkill)
-{
-	lockdep_assert_held(&rfkill_global_mutex);
-
-	if (!rfkill->need_sync)
-		return;
-
-	rfkill_set_block(rfkill, rfkill_global_states[rfkill->type].cur);
-	rfkill->need_sync = false;
 }
 
 static void rfkill_update_global_state(enum rfkill_type type, bool blocked)
@@ -742,10 +730,6 @@ static ssize_t soft_show(struct device *dev, struct device_attribute *attr,
 {
 	struct rfkill *rfkill = to_rfkill(dev);
 
-	mutex_lock(&rfkill_global_mutex);
-	rfkill_sync(rfkill);
-	mutex_unlock(&rfkill_global_mutex);
-
 	return sysfs_emit(buf, "%d\n", (rfkill->state & RFKILL_BLOCK_SW) ? 1 : 0);
 }
 
@@ -767,7 +751,6 @@ static ssize_t soft_store(struct device *dev, struct device_attribute *attr,
 		return -EINVAL;
 
 	mutex_lock(&rfkill_global_mutex);
-	rfkill_sync(rfkill);
 	rfkill_set_block(rfkill, state);
 	mutex_unlock(&rfkill_global_mutex);
 
@@ -800,10 +783,6 @@ static ssize_t state_show(struct device *dev, struct device_attribute *attr,
 {
 	struct rfkill *rfkill = to_rfkill(dev);
 
-	mutex_lock(&rfkill_global_mutex);
-	rfkill_sync(rfkill);
-	mutex_unlock(&rfkill_global_mutex);
-
 	return sysfs_emit(buf, "%d\n", user_state_from_blocked(rfkill->state));
 }
 
@@ -826,7 +805,6 @@ static ssize_t state_store(struct device *dev, struct device_attribute *attr,
 		return -EINVAL;
 
 	mutex_lock(&rfkill_global_mutex);
-	rfkill_sync(rfkill);
 	rfkill_set_block(rfkill, state == RFKILL_USER_STATE_SOFT_BLOCKED);
 	mutex_unlock(&rfkill_global_mutex);
 
@@ -1054,10 +1032,14 @@ static void rfkill_uevent_work(struct work_struct *work)
 
 static void rfkill_sync_work(struct work_struct *work)
 {
-	struct rfkill *rfkill = container_of(work, struct rfkill, sync_work);
+	struct rfkill *rfkill;
+	bool cur;
+
+	rfkill = container_of(work, struct rfkill, sync_work);
 
 	mutex_lock(&rfkill_global_mutex);
-	rfkill_sync(rfkill);
+	cur = rfkill_global_states[rfkill->type].cur;
+	rfkill_set_block(rfkill, cur);
 	mutex_unlock(&rfkill_global_mutex);
 }
 
@@ -1105,7 +1087,6 @@ int __must_check rfkill_register(struct rfkill *rfkill)
 			round_jiffies_relative(POLL_INTERVAL));
 
 	if (!rfkill->persistent || rfkill_epo_lock_active) {
-		rfkill->need_sync = true;
 		schedule_work(&rfkill->sync_work);
 	} else {
 #ifdef CONFIG_RFKILL_INPUT
@@ -1189,7 +1170,6 @@ static int rfkill_fop_open(struct inode *inode, struct file *file)
 		ev = kzalloc(sizeof(*ev), GFP_KERNEL);
 		if (!ev)
 			goto free;
-		rfkill_sync(rfkill);
 		rfkill_fill_event(&ev->ev, rfkill, RFKILL_OP_ADD);
 		mutex_lock(&data->mtx);
 		list_add_tail(&ev->list, &data->events);
